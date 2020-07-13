3f31c0add2ca0bd1ee7c42d82e7ff609fc6af1ab
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:39:42 -0300

    feat: progress display component

---
+++ src/app/home/happiness/components/circularProgress.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-progress-circular
+    :rotate="360"
+    :size="110"
+    :width="10"
+    :value="happinessPercent"
+    :color="happinessColor"
+  >
+    <FaceMoment color="grey"/>
+  </v-progress-circular>
+</template>
+
+<script>
+import FaceMoment from './faceMoment.vue'
+export default {
+  name: 'CircularProgress',
+  components: {
+    FaceMoment
+  },
+  computed: {
+    happinessPercent () {
+      const multiplyingFactor = 10
+      return this.$store.getters.happinessScore * multiplyingFactor
+    },
+    happinessColor () {
+      return this.$store.getters.happiness.color
+    }
+  }
+}
+</script>
