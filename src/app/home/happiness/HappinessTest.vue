aa9daa87aa9677b6d91e80cdfe6e3340029d926e
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:48:15 -0300

    feat: component that displays and directs to jokes

---
+++ src/app/home/happiness/HappinessTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppContainer>
+    <CardTest slot="content" />
+  </AppContainer>
+</template>
+
+<script>
+import AppContainer from './components/appContainer.vue'
+import CardTest from './components/cardTest.vue'
+export default {
+  name: 'HappinessTest',
+  components: {
+    AppContainer,
+    CardTest
+  }
+}
+</script>
---
+++ src/app/home/happiness/components/cardTest.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="card-test" class="d-flex flex-column align-center">
+    <HappinessInformation/>
+    <FaceMoment big/>
+    <BtnToHapinessMachine/>
+  </section>
+</template>
+
+<script>
+import HappinessInformation from './happinessInformation.vue'
+import BtnToHapinessMachine from './form/btnToHapinessMachine'
+import FaceMoment from './faceMoment.vue'
+export default {
+  name: 'CardTest',
+  components: {
+    HappinessInformation,
+    FaceMoment,
+    BtnToHapinessMachine
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  #card-test
+    max-width 350px
+</style>
