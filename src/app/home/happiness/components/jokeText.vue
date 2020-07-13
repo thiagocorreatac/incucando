fe8c7d1b8d161f13d3f06b216aa4e588fa1e4e21
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:42:24 -0300

    feat: jokes display component

---
+++ src/app/home/happiness/components/jokeText.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card-text class="text-justify font-weight-bold mt-6">
+    {{joke}}
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  name: 'JokeText',
+  computed: {
+    joke () {
+      return this.$store.getters.joke.text
+    }
+  }
+}
+</script>
