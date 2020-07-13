3ef67df70c3d5a65756210b282a9535c26d7f736
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:34:40 -0300

    feat: main page

---
+++ src/app/home/happiness/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container
+    class="fill-height"
+    fluid
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <HelloUser/>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import HelloUser from './components/helloUser'
+export default {
+  name: 'Main',
+  components: {
+    HelloUser
+  }
+}
+</script>
