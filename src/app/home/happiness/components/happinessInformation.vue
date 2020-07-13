f8f24bd9770f5858cf82a2cdc6b840c35d1e5fd5
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:41:42 -0300

    feat: analytics display component

---
+++ src/app/home/happiness/components/happinessInformation.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>Análise: <b>{{emotion.title}}</b></h1>
+    <p>{{emotion.description}}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HappinessInformation',
+  computed: {
+    emotion () {
+      return this.$store.getters.happiness
+    }
+  }
+}
+</script>
