f1b7ed0ef8bb53dabefa7c63954a653bdddaff4e
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:45:05 -0300

    feat: component to start and complete the analysis

---
+++ src/app/home/happiness/components/form/btnToHapinessMachine.vue
@@ -0,0 +1,32 @@
+<template>
+  <section id="to-machine-action" class="mt-10">
+    <div class="row">
+    <v-btn
+      v-if="happinessScore === 10"
+      color="black"
+      class="yellow--text"
+      to="/"
+    >
+      Chegar de brincar, vou trabalhar
+    </v-btn>
+    <v-btn
+      v-if="happinessScore < 10"
+      color="black"
+      class="yellow--text"
+      to="/machine"
+    >
+      Quero ficar feliz
+    </v-btn>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'BtnToHapinessMachine',
+  computed: {
+    happinessScore () {
+      return this.$store.getters.happinessScore
+    }
+  }
+}
+</script>
