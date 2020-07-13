faccfb7fdfade27a816679aa82538b34b97340f7
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:37:25 -0300

    feat: standard structure component

---
+++ src/app/home/happiness/components/appContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container
+    class="fill-height"
+    fluid
+    :style="'background-color:' + colorBackground"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <slot name="content"/>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AppContainer',
+  computed: {
+    colorBackground () {
+      return this.$store.getters.happiness.color
+    }
+  }
+}
+</script>
