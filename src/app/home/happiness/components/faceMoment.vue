ad80c5846c2e1ba73ea7604fcc97528941242e64
Thiago Correa <thiagocorrea@sismatech.com>
2020-07-13 05:39:02 -0300

    feat: emoticon display component

---
+++ src/app/home/happiness/components/faceMoment.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="face-moment">
+    <v-icon :color="color" v-bind:class="{ 'big-face': big }">{{face}}</v-icon>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'FaceMoment',
+  computed: {
+    face () {
+      return this.$store.getters.happiness.face
+    }
+  },
+  props: {
+    color: {
+      type: String,
+      default: 'black'
+    },
+    big: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .v-icon
+    font-size 80px
+  .big-face
+    font-size 140px !important
+</style>
